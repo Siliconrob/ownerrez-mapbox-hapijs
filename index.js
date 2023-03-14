ad6805f1b3c97bbe2d91e999930c749e14116a04
Glitch (glitch-hello-node) <none>
2023-03-14 02:21:54 +0000

    🏦🎽 Checkpoint ./index.js:4054/1288 ./public/map.html:4054/12

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const Path = require('path');
 
 const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: '0.0.0.0'
     });
+  
+    await server.register(require('@hapi/inert'));
+    
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return h.file('public/map.html');
+        }
+    });
+  
 
     await server.start();
     console.log('Server running on %s', server.info.uri);

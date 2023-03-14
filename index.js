5132526678dc9dcee323ad52505f28f597f71f2b
Glitch (glitch-hello-node) <none>
2023-03-14 03:31:03 +0000

    🏩🍄 Checkpoint ./index.js:4054/2275 ./package.json:4054/1887

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
-const Path = require('path');
-const superagent = require('superagent');
+const Hapi = require("@hapi/hapi");
+const Path = require("path");
+const superagent = require("superagent");
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "0.0.0.0",
+  });
 
-    const server = Hapi.server({
-        port: 3000,
-        host: '0.0.0.0'
-    });
-  
-    await server.register(require('@hapi/inert'));
-    
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return h.file('public/map.html');
-        }
-    });
-  
-    server.route({
-        method: 'GET',
-        path: '/data',
-        handler: async (request, h) => {
-          
-            const response = await superagent.get("www.google.com");
-            const text = await response.body;
-            console.log(text)
-          
-            return process.env.mapkey;
-        }
-    });
-  
+  await server.register(require("@hapi/inert"));
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return h.file("public/map.html");
+    },
+  });
+
+  server.route({
+    method: "GET",
+    path: "/data",
+    handler: async (request, h) => {
+      const url = "https://secure.ownerreservations.com/api/properties/lookup";
+      const response = await superagent.get("google.com");
+      const text = await response.body;
+      console.log(text);
+
+      return process.env.mapkey;
+    },
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
 };
 
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
 });
 
 init();
